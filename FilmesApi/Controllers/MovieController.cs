88e0734f758196f4845bf396b1b48a088199123f
LuanHSL <luansilvalemes@gmail.com>
2026-02-16 04:53:44 -0300

    first commit

---
+++ FilmesApi/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MovieController : ControllerBase
+{
+    private static List<Movie> movieList = [];
+
+    [HttpPost]
+    public void AddMovie([FromBody] Movie movie)
+    {
+        movieList.Add(movie);
+        Console.WriteLine("--------Filme--------");
+        Console.WriteLine($"Titulo: {movie.Title}");
+        Console.WriteLine($"Genero: {movie.Genre}");
+        Console.WriteLine($"Duracao: {movie.Duration}");
+
+    }
+}
