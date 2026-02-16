a69c47a69ab7c88259f232833ea8ab764e1b2a7a
LuanHSL <luansilvalemes@gmail.com>
2026-02-16 05:22:56 -0300

    feat: adding and getting movie

--- FilmesApi/Controllers/MovieController.cs
+++ FilmesApi/Controllers/MovieController.cs
@@ -8,15 +8,27 @@ namespace FilmesApi.Controllers;
 public class MovieController : ControllerBase
 {
     private static List<Movie> movieList = [];
+    private static int id = 0;
 
     [HttpPost]
-    public void AddMovie([FromBody] Movie movie)
+    public IActionResult AddMovie([FromBody] Movie movie)
     {
+        movie.Id = id++;
         movieList.Add(movie);
-        Console.WriteLine("--------Filme--------");
-        Console.WriteLine($"Titulo: {movie.Title}");
-        Console.WriteLine($"Genero: {movie.Genre}");
-        Console.WriteLine($"Duracao: {movie.Duration}");
+        return CreatedAtAction(nameof(GetMovieById), new { id = movie.Id }, movie);
+    }
 
+    [HttpGet]
+    public IEnumerable<Movie> GetMovie([FromQuery] int skip = 0, [FromQuery] int take = 10)
+    {
+        return movieList.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetMovieById(int id)
+    {
+        var movie = movieList.FirstOrDefault(movie => movie.Id == id);
+        if (movie == null) return NotFound();
+        return Ok(movie);
     }
 }
--- FilmesApi/Models/Movie.cs
+++ FilmesApi/Models/Movie.cs
@@ -1,8 +1,16 @@
-﻿namespace FilmesApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
 
 public class Movie
 {
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Title is required")]
     public string Title { get; set; }
+    [Required(ErrorMessage = "Genre is required")]
+    [MaxLength(50, ErrorMessage = "Genre lenght cannot exceed 50 characters")]
     public string Genre { get; set; }
+    [Required(ErrorMessage = "Duration is required")]
+    [Range(70, 600, ErrorMessage = "Duration must be between 70 and 600 minutes")]
     public int Duration { get; set; }
 }
